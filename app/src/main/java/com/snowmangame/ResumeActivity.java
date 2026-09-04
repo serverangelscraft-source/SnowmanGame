ea0b7238223da3d620898fc3d795a403a02c9bc4
serverangelscraft-source <serverangelscraft@gmail.com>
2026-09-04 18:02:51 +0300

    Resume unfinished school integrations

--- app/src/main/java/com/snowmangame/ResumeActivity.java
+++ app/src/main/java/com/snowmangame/ResumeActivity.java
@@ -35,7 +35,8 @@ public class ResumeActivity extends Activity {
                 || p.contains("year_builds_6")
                 || p.contains("year_builds_7");
 
-        if(schoolWeekStarted) next=new Intent(this,IntegratedSchoolWeekActivity.class);
+        if(SchoolIntegrationSession.pending(p)) next=SchoolIntegrationSession.resumeIntent(this,p);
+        else if(schoolWeekStarted) next=new Intent(this,IntegratedSchoolWeekActivity.class);
         else if(schoolUnlocked) next=new Intent(this,SchoolActivity.class);
         else if(hasProgress) next=new Intent(this,MainActivity.class);
         else next=new Intent(this,IntroActivity.class);
